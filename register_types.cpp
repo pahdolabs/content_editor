109740d1f8f41458f82e0f338824de0dd5dc859c
Francois Belair <razoric480@gmail.com>
2022-05-23 21:02:43 -0400

    Add more boilerplate

---
+++ register_types.cpp
@@ -0,0 +1,10 @@
+#include "register_types.h"
+
+#include "core/class_db.h"
+
+void register_content_editor_types() {
+	
+}
+
+void unregister_content_editor_types() {
+}
---
+++ register_types.h
@@ -0,0 +1,2 @@
+void register_content_editor_types();
+void unregister_content_editor_types();
